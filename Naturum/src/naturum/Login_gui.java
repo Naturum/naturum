638560d221d88a2e3980f51638ae09f0b1d2c13a
Wrynaft <wrynaft@gmail.com>
2023-10-29 22:45:33 +0800

    Updated initial GUI and scene controller

--- Naturum/src/naturum/Login_gui.java
+++ Naturum/src/naturum/Login_gui.java
@@ -3,63 +3,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package naturum;   
-import javafx.application.Application;  
-//import javafx.event.ActionEvent;  
-//import javafx.event.EventHandler;  
-import javafx.scene.Scene;  
-import javafx.scene.control.Button;  
+import javafx.application.Application;   
+import javafx.fxml.FXMLLoader;  
 import javafx.stage.Stage;  
-import javafx.scene.layout.GridPane;  
-import javafx.geometry.Pos;
-import javafx.geometry.Insets;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
-import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 public class Login_gui extends Application{  
   
     @Override  
     public void start(Stage primaryStage) throws Exception {  
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(10, 10, 10, 10));
-        
-        Text sceneTitle = new Text("Login");
-        sceneTitle.setTextAlignment(TextAlignment.CENTER);
-        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 30));
-        grid.add(sceneTitle, 1, 1, 2, 1);
-        
-        Image image = new Image("assets/naturum-logo.png");
-        ImageView imageView = new ImageView(image);
-        imageView.setFitHeight(100);
-        imageView.setFitWidth(66);
-        grid.add(imageView, 1, 0, 2, 1);
-        
-        Label userName = new Label("Email:");
-        grid.add(userName, 0, 3);
-
-        TextField userTextField = new TextField();
-        grid.add(userTextField, 1, 3);
-
-        Label pw = new Label("Password:");
-        grid.add(pw, 0, 4);
-
-        PasswordField passwordBox = new PasswordField();
-        grid.add(passwordBox, 1, 4);
-        
-        Button btn = new Button("Sign in");
-        HBox hbBtn = new HBox(10);
-        hbBtn.setAlignment(Pos.CENTER);
-        hbBtn.getChildren().add(btn);
-        grid.add(hbBtn, 1, 5);
-        
-        Scene scene=new Scene(grid,600,400);    
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Scene scene = new Scene(root, 600, 400);
         primaryStage.setTitle("NaturUM");  
         primaryStage.setScene(scene);  
         primaryStage.show();  
---
+++ Naturum/src/naturum/SceneController.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package naturum;
+
+/**
+ *
+ * @author Ryan Chin
+ */
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+public class SceneController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
+    public void switchToLogin(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToMainpage(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("mainpage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root, 800, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
